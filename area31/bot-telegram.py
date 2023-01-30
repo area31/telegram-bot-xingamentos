824d06ac202b9cd99d8fce5c773d3b490b2b25b4
root <raphaelbastos@hackstore.com.br>
2023-01-30 09:14:35 -0300

    corrige o /ajuda

--- area31/bot-telegram.py
+++ area31/bot-telegram.py
@@ -144,7 +144,7 @@ def help_message(message):
     help_text += '/euro - Exibe a cotação do euro em reais\n'
     help_text += '/btc - Exibe a cotação do Bitcoin em dolares\n'
     help_text += '/xmr - Exibe a cotação do Monero em dolares\n'
-    help_text += '/real - Comando desnecessário pelo óbvio, mas tente executar pra ver...'
+    help_text += '/real - Comando desnecessário pelo óbvio, mas tente executar pra ver...\n'
     help_text += '/search - Exibe resultados de busca no Google'
     bot.send_message(message.chat.id, help_text)
 
