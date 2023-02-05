07217186a1eadc09c53b9bc6372943a7bac046d6
root <raphaelbastos@hackstore.com.br>
2023-02-04 22:02:18 -0300

    upgrade da api davinci 002 pra 003

--- area31/bot-telegram.py
+++ area31/bot-telegram.py
@@ -86,7 +86,7 @@ def respond(message):
 
     try:
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="text-davinci-003",
             prompt=prompt,
             max_tokens=1024,
             n=1,
